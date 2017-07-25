faa194ba5368c509d908fe6841281f85a01ba87d
elizabeth19-meet <elizabeth19@meet.mit.edu>
2017-07-25 15:46:35 +0200

    Stopping here

---
+++ Hellow World.py
@@ -0,0 +1,12 @@
+print("Γειά σου Κόσμε")
+print("Olá Mundo")
+print("Ahoj světe")
+print("안녕 세상")
+print("नमस्ते दुनिया ")
+print("Salamu, Dunia ")
+print("Γειά σου Κόσμε")
+print("Привет мир")
+print("שלום עולם")
+print("Прывітанне Сусвет")
+ 
+
